a592fa3b52532c7075e585abd88c80d56e339aad
Hugo Müller-Downing <github@hu.md>
2021-09-03 20:11:35 +1000

    Add openring generated feeds

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+go install git.sr.ht/~sircmpwn/openring@latest
+
+$GOPATH/bin/openring \
+  -n 5 \
+  -p 1 \
+  -l 0 \
+  -s https://kevq.uk/feed/ \
+  -s https://joelchrono12.ml/feed.xml \
+  -s https://mxb.dev/feed.xml \
+  -s https://ndanes.com/feed.xml \
+  -s https://www.renlord.com/index.xml \
+  -s https://rusingh.com/feed/ \
+  -s https://stanislas.blog/atom.xml \
+  -s https://icyphox.sh/blog/feed.xml \
+  -s https://nicholas.cloud/rss/ \
+  -s https://stefanzweifel.io/rss.xml \
+  -s https://healeycodes.com/feed.xml \
+  -s https://peppe.rs/index.xml \
+  < openring_in.html \
+  > ./layouts/partials/openring_out.html
+
+hugo --minify --baseURL="https://hugo.md/"
